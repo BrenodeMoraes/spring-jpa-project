e73e03b0037f4a29ae4541ff65764df8509cbb59
tildo <breno.moraes1809@gmail.com>
2024-05-08 22:07:13 -0300

    project created

---
+++ src/main/java/org/devbrenomoraes/springjpa_project/SpringjpaProjectApplication.java
@@ -0,0 +1,13 @@
+package org.devbrenomoraes.springjpa_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringjpaProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringjpaProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/devbrenomoraes/springjpa_project/SpringjpaProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package org.devbrenomoraes.springjpa_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringjpaProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
